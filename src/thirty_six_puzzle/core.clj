b8c11c94b028003885bc837e2685684964dd9779
Dennis Lipovsky <dennis@yodle.com>
2015-05-24 21:51:15 -0400

    helper functions to create and print a board

--- src/thirty_six_puzzle/core.clj
+++ src/thirty_six_puzzle/core.clj
@@ -1,6 +1,20 @@
 (ns thirty-six-puzzle.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def field [[5 3 2 1 4 6]
+            [4 1 6 2 5 3]
+            [6 5 3 4 1 2]
+            [1 2 5 3 6 4]
+            [2 4 1 6 3 5]
+            [3 6 4 5 2 1]])
+
+(def empty-board (vec (repeat 6 (vec (repeat 6 :none)))))
+
+(def colors [:green :blue :red :yellow :purple :orange])
+
+(def color-string {:none "-" :green "G" :blue "B" :red "R" :yellow "Y" :purple "P" :orange "O"})
+
+(defn place-cube [board row col color]
+  (update-in board [row col] (fn [_] color)))
+
+(defn print-board [board]
+  (clojure.string/join "\n" (map #(apply str (map color-string %)) board)))
