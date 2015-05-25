8e4afef0e63873bccd52a27df66c563793bf85a8
Dennis Lipovsky <dennis@yodle.com>
2015-05-24 23:09:48 -0400

    working on the functions to manipulate the board and determine validity of a move

--- src/thirty_six_puzzle/core.clj
+++ src/thirty_six_puzzle/core.clj
@@ -1,20 +1,44 @@
 (ns thirty-six-puzzle.core)
 
-(def field [[5 3 2 1 4 6]
-            [4 1 6 2 5 3]
-            [6 5 3 4 1 2]
-            [1 2 5 3 6 4]
-            [2 4 1 6 3 5]
-            [3 6 4 5 2 1]])
-
-(def empty-board (vec (repeat 6 (vec (repeat 6 :none)))))
+(def empty-board [[{:height 5 :color :none} {:height 3 :color :none} {:height 2 :color :none} {:height 1 :color :none} {:height 4 :color :none} {:height 6 :color :none}]
+                  [{:height 4 :color :none} {:height 1 :color :none} {:height 6 :color :none} {:height 2 :color :none} {:height 5 :color :none} {:height 3 :color :none}]
+                  [{:height 6 :color :none} {:height 5 :color :none} {:height 3 :color :none} {:height 4 :color :none} {:height 1 :color :none} {:height 2 :color :none}]
+                  [{:height 1 :color :none} {:height 2 :color :none} {:height 5 :color :none} {:height 3 :color :none} {:height 6 :color :none} {:height 4 :color :none}]
+                  [{:height 2 :color :none} {:height 4 :color :none} {:height 1 :color :none} {:height 6 :color :none} {:height 3 :color :none} {:height 5 :color :none}]
+                  [{:height 3 :color :none} {:height 6 :color :none} {:height 4 :color :none} {:height 5 :color :none} {:height 2 :color :none} {:height 1 :color :none}]])
 
 (def colors [:green :blue :red :yellow :purple :orange])
 
-(def color-string {:none "-" :green "G" :blue "B" :red "R" :yellow "Y" :purple "P" :orange "O"})
+(defn color-string [m] (get {:none "-" :green "G" :blue "B" :red "R" :yellow "Y" :purple "P" :orange "O"} (:color m)))
 
 (defn place-cube [board row col color]
-  (update-in board [row col] (fn [_] color)))
+  (update-in board [row col :color] (fn [_] color)))
 
-(defn print-board [board]
+(defn printable-board [board]
   (clojure.string/join "\n" (map #(apply str (map color-string %)) board)))
+
+(defn transpose [m]
+  (apply mapv vector m))
+
+(defn row-has-height [row height]
+  (some #(and
+          (= height (:height %))
+          (= :none (:color %))) row))
+
+(defn row-has-color [row color]
+  (some #(= color (:color %)) row))
+
+(defn valid-heights [board height]
+  (map #(row-has-height % height) board))
+
+(defn valid-color [board color]
+  (map #(not (row-has-color % color)) board))
+
+(defn valid-row [position-data]
+  (map #(every? identity %) (apply mapv vector position-data)))
+
+(defn valid-rows [board color height]
+  (let [valid-heights (valid-heights board height)
+        valid-color-row (valid-color board color)
+        valid-color-col (valid-color (transpose board) color)]
+    [valid-heights valid-color-row valid-color-col]))
